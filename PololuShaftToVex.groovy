d8c5b7fad7588f270a999dbd5138df4888c805ce
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-11 17:36:00 -0400

    adding real measurments

--- PololuShaftToVex.groovy
+++ PololuShaftToVex.groovy
@@ -1,14 +1,26 @@
-CSG pshaft =new Cylinder(2,2,11,(int)30).toCSG() // a one line Cylinder
-CSG flat = new Cube(	4,// X dimention
-			3.4,// Y dimention
+double measuredVSize = 2.75
+double vshaftSize = 3.12
+double finalV = vshaftSize+(vshaftSize-measuredVSize)
+
+double measuredpSize = 3.46/2
+double pshaftSize = 2
+double finalp = pshaftSize+(pshaftSize-measuredpSize)
+
+double measuredfSize = 3.15
+double fshaftSize = 3.4
+double finalf = fshaftSize+(fshaftSize-measuredfSize)
+
+CSG pshaft =new Cylinder(finalp,finalp,11,(int)30).toCSG() // a one line Cylinder
+CSG flat = new Cube(	finalp*2,// X dimention
+			finalf,// Y dimention
 			11//  Z dimention
 			).toCSG()
 			.toYMin()
 			.toZMin()
 			.movey(pshaft.getMinY())
 
-CSG xshaft =  new RoundedCube(	3.12,// X dimention
-				3.12,// Y dimention
+CSG vshaft =  new RoundedCube(	finalV,// X dimention
+				finalV,// Y dimention
 				11//  Z dimention
 				)
 				.cornerRadius(0.5)// sets the radius of the corner
@@ -21,7 +33,7 @@ CSG coupler = new Cube(	7.5,// X dimention
 			7.5,// Y dimention
 			20//  Z dimention
 			).toCSG()
-			.difference([pshaft,xshaft])
+			.difference([pshaft,vshaft])
 			.rotx(90)
 			.toZMin()
 return [coupler]
