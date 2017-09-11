652e26a191c126b7841a1e7f335a9d139fa22c72
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-11 16:55:01 -0400

    vex to pololu shaft adapter

--- PololuShaftToVex.groovy
+++ PololuShaftToVex.groovy
@@ -1,1 +1,27 @@
-//Your code here
+CSG pshaft =new Cylinder(2,2,11,(int)30).toCSG() // a one line Cylinder
+CSG flat = new Cube(	4,// X dimention
+			3.4,// Y dimention
+			11//  Z dimention
+			).toCSG()
+			.toYMin()
+			.toZMin()
+			.movey(pshaft.getMinY())
+
+CSG xshaft =  new RoundedCube(	3.12,// X dimention
+				3.12,// Y dimention
+				11//  Z dimention
+				)
+				.cornerRadius(0.5)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+				.toZMax()
+				.movez(-1)
+pshaft=pshaft.intersect(flat)
+
+CSG coupler = new Cube(	7.5,// X dimention
+			7.5,// Y dimention
+			20//  Z dimention
+			).toCSG()
+			.difference([pshaft,xshaft])
+			.rotx(90)
+			.toZMin()
+return [coupler]
