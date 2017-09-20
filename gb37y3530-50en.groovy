42dc91ee688b804c4dce6f03a111cad7189cf37a
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-20 16:11:02 -0400

    forming the whole motor

--- gb37y3530-50en.groovy
+++ gb37y3530-50en.groovy
@@ -1,5 +1,17 @@
+// Measurments from http://image.dfrobot.com/image/data/FIT0185/FIT0185_Dimension.PNG
 
+double boltHoleDiameter = 31
+double shaftCenterOffset = 7.0
+double shaftCollarDiameter = 12.0/2.0
+double shaftCollarHeight = 5.8 
+double motorBodyRadius = 37.0/2.0
+double bodyLength=75.0
+CSG body =new Cylinder(motorBodyRadius,motorBodyRadius,bodyLength,(int)30).toCSG() // a one line Cylinder
+			.toZMax()
+CSG collar =new Cylinder(shaftCollarDiameter,shaftCollarDiameter,shaftCollarHeight,(int)30).toCSG() // a one line Cylinder
 CSG bolt = Vitamins.get("capScrew","M3")
+			.rotx(180)
+			.movey(boltHoleDiameter/2)
 
 pshaft=(CSG)ScriptingEngine
 	                    .gitScriptRun(
@@ -7,5 +19,11 @@ pshaft=(CSG)ScriptingEngine
 	                              "dShaft.groovy" , // file to load
 	                              [6.0,5.34,21]
                         )
-
-return [pshaft,bolt]
+                        .union(collar)
+					.movex(shaftCenterOffset)
+CSG bolts = bolt
+for(int i=60;i<360;i+=60){
+	bolts=bolts.union(bolt.rotz(i))
+}
+CSG wholeMotor = CSG.unionAll([pshaft,bolts,body])
+return wholeMotor
