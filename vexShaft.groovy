09d6df68e433cbe5ac93db87463c26d36306ec8a
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-20 17:28:11 -0400

    making a parametric vex shaft

--- vexShaft.groovy
+++ vexShaft.groovy
@@ -1,1 +1,17 @@
-//Your code here
+if(args== null)
+	args = [11]
+
+LengthParameter printerOffset = new LengthParameter("printerOffset",0.25,[2,0.001])
+
+double vshaftSize = 3.12
+double finalV = vshaftSize+(printerOffset.getMM()*1.5)
+
+
+CSG vshaft =  new RoundedCube(	finalV,// X dimention
+				finalV,// Y dimention
+				args.get(0)//  Z dimention
+				)
+				.cornerRadius(0.5)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+				
+return 	vshaft			
