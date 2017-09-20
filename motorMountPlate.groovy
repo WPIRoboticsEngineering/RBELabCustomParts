2a597a1488ee79b81bc12e5f6164c0f431abae13
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-20 17:55:12 -0400

    makeing a mount bracket for the pololu to vex grid

--- motorMountPlate.groovy
+++ motorMountPlate.groovy
@@ -20,5 +20,33 @@ mesh=mesh.movex(vexHoleSpacing*gearRadiusIndex)
 CSG spacer = Vitamins.get("vexSpacer","oneEighth")
 			.toZMax()
 			.movex(vexHoleSpacing*gearRadiusIndex)
-			
-return [motor,gear,mesh,spacer]
+HashMap<String,Object> vexSpacerConfig = Vitamins.getConfiguration( "vexSpacer","oneEighth")
+double innerRadius = vexSpacerConfig.innerDiameter/2
+CSG vexHole = new Cylinder(innerRadius, innerRadius, vexHoleSpacing*2, 10).toCSG()
+				.toZMax()
+
+CSG vexHoleSet = vexHole
+
+int width = 4
+int length = 2
+
+for(int i=0;i<length;i++){
+	for(int j=-(width/2);j<(width/2+1);j++){
+		vexHoleSet=vexHoleSet.union(vexHole
+					.movex((i*vexHoleSpacing)+vexHoleSpacing)
+					.movey(j*vexHoleSpacing)
+					)
+	}
+}
+
+CSG mountPlate = new Cube(vexHoleSpacing*(length+2.5),vexHoleSpacing*(width+1),vexHoleSpacing/2).toCSG()
+				.toZMax()
+				.toXMin()
+				.movex(-vexHoleSpacing*2)
+				.difference([motor,vexHoleSet])
+mountPlate.setMfg( {toMfg ->
+		toMfg.rotx(180)
+			.toZMin()
+})
+				
+return [motor,gear,mesh,spacer,mountPlate]
