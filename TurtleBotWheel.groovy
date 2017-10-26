e422f352ff9969635c26349f820af93d8dc17911
Kevin Harrington <mad.hephaestus@gmail.com>
2017-10-26 13:00:46 -0400

    Create TurtleBotWheel.groovy

---
+++ TurtleBotWheel.groovy
@@ -0,0 +1,29 @@
+CSG simpleSyntax =new Cylinder(10,10,40,(int)30).toCSG() // a one line Cylinder
+
+double wheelRadius = 33
+wheelHeight=17.6
+double radius = 6.0
+CSG profile = new RoundedCube(	wheelRadius,// X dimention
+				radius,// Y dimention
+				wheelHeight//  Z dimention
+				)
+				.cornerRadius(radius)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+				.toXMin()
+				.toZMin()
+
+//create a Cylinder
+height = 22.5
+CSG wheel_body = CSG.unionAll(
+		Extrude.revolve(profile,(double)0,(int)40)
+		)
+                      		
+CSG pin = new Cylinder(5, // Radius at the bottom
+                       5, // Radius at the top
+                       height, // Height
+                       (int)30 //resolution
+                       ).toCSG()
+
+CSG wheel = wheel_body.difference(pin)
+
+ return [wheel ]
