0cfc2cd861c6cd5a7f911e4386df371980b291f6
Kevin Harrington <mad.hephaestus@gmail.com>
2018-08-28 19:07:11 -0400

    plastic printable bearing

--- 695zzBushing.groovy
+++ 695zzBushing.groovy
@@ -1,1 +1,18 @@
-//Your code here
+def offset = 0.6
+def h=4
+def outerrad = (13-offset+0.1)/2
+def innerrad=(5+offset)/2
+def halfRad = outerrad - ((outerrad-innerrad)/2)
+def quaterRad= outerrad - ((outerrad-innerrad)/4)
+CSG outer =new Cylinder(outerrad,h).toCSG() // a one line Cylinder
+CSG hole =new Cylinder(innerrad,h).toCSG() // a one line Cylinder
+CSG profile = new Cylinder(halfRad, // Radius at the bottom
+                      		quaterRad, // Radius at the top
+                      		h/2, // Height
+                      		(int)30 //resolution
+                      		).toCSG()//convert to CSG to display   
+profile=profile.union(profile.rotx(180).movez(h))
+
+def outerRace =outer.difference( profile.makeKeepaway(offset))
+def innerRace = outer.intersect( profile).difference(hole)
+return CSG.unionAll([outerRace,innerRace])
