d3624c0554ee0fd7ef40f38ac259857c61e34feb
Kevin Harrington <mad.hephaestus@gmail.com>
2018-08-28 20:39:50 -0400

    centering the flange

--- 695zzBushing.groovy
+++ 695zzBushing.groovy
@@ -6,13 +6,13 @@ def halfRad = outerrad - ((outerrad-innerrad)/2)
 def quaterRad= outerrad - ((outerrad-innerrad)/4)
 CSG outer =new Cylinder(outerrad,h).toCSG() // a one line Cylinder
 CSG hole =new Cylinder(innerrad,h).toCSG() // a one line Cylinder
-CSG profile = new Cylinder(halfRad, // Radius at the bottom
-                      		quaterRad, // Radius at the top
+CSG profile = new Cylinder(halfRad-offset, // Radius at the bottom
+                      		quaterRad-offset, // Radius at the top
                       		h/2, // Height
                       		(int)30 //resolution
                       		).toCSG()//convert to CSG to display   
 profile=profile.union(profile.rotx(180).movez(h))
 
-def outerRace =outer.difference( profile.makeKeepaway(offset))
+def outerRace =outer.difference( profile.makeKeepaway(offset*2).movez(offset))
 def innerRace = outer.intersect( profile).difference(hole)
-return CSG.unionAll([outerRace,innerRace])
+return[outerRace,innerRace]
