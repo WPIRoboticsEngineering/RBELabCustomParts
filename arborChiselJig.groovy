1128e89546f3c13b496039b7fd2904ed5d3d7de9
Kevin Harrington <mad.hephaestus@gmail.com>
2019-01-24 11:00:20 -0500

    bigger slot

--- arborChiselJig.groovy
+++ arborChiselJig.groovy
@@ -1,1 +1,10 @@
-//Your code here
+def bracket = new Cube(40,40,40).toCSG()
+			.toZMin()
+
+bracket = bracket.difference(new Cube(25,25,bracket.getTotalZ()-17).toCSG()
+			.toZMax()
+			.movez(bracket.getMaxZ()))
+def slot=new Cube(0.7,9.5,bracket.getTotalZ()).toCSG()
+		.toZMin()
+bracket=bracket.difference(slot)
+return bracket
