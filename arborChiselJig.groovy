7bc5e9b637a9f63172e70044823d173b588b64f1
Kevin Harrington <mad.hephaestus@gmail.com>
2019-01-24 11:03:23 -0500

    shorter lip

--- arborChiselJig.groovy
+++ arborChiselJig.groovy
@@ -1,10 +1,10 @@
-def bracket = new Cube(40,40,40).toCSG()
+def bracket = new Cube(40,40,20).toCSG()
 			.toZMin()
 
 bracket = bracket.difference(new Cube(25,25,bracket.getTotalZ()-17).toCSG()
 			.toZMax()
 			.movez(bracket.getMaxZ()))
-def slot=new Cube(0.7,9.5,bracket.getTotalZ()).toCSG()
+def slot=new Cube(0.63,9.5,bracket.getTotalZ()).toCSG()
 		.toZMin()
 bracket=bracket.difference(slot)
 return bracket
