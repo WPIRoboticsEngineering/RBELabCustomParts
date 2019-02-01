005686ddfaeb08aa5c85c1c119ae005af0e1aeea
Kevin Harrington <mad.hephaestus@gmail.com>
2019-02-01 10:29:52 -0500

    making more room for the nutserts

--- arborChiselJig.groovy
+++ arborChiselJig.groovy
@@ -1,13 +1,13 @@
-def bracket = new Cube(40,40,30).toCSG()
+def bracket = new Cube(50,50,30).toCSG()
 			.toZMin()
 
 bracket = bracket.difference(new Cube(25,25,bracket.getTotalZ()-17).toCSG()
 			.toZMax()
 			.movez(bracket.getMaxZ()))
-def slot=new Cube(0.63,10.5,bracket.getTotalZ()).toCSG()
+def slot=new Cube(0.93,10.5,bracket.getTotalZ()).toCSG()
 		.toZMin()
 
-def setscrew = new Cylinder(5.6/2,40).toCSG()
+def setscrew = new Cylinder(5.9/2,40).toCSG()
 			.movez(-bracket.getTotalX()/2)
 			.roty(90)
 			.toZMin()
