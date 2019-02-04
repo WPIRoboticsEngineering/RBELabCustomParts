295582a2fcee6258c188057de3991c1b222de569
Kevin Harrington <mad.hephaestus@gmail.com>
2019-02-04 09:21:01 -0500

    dimentions

--- arborChiselJig.groovy
+++ arborChiselJig.groovy
@@ -1,13 +1,13 @@
 def bracket = new Cube(50,50,30).toCSG()
 			.toZMin()
 
-bracket = bracket.difference(new Cube(25,25,bracket.getTotalZ()-17).toCSG()
+bracket = bracket.difference(new Cube(25.5,25.5,bracket.getTotalZ()-17).toCSG()
 			.toZMax()
 			.movez(bracket.getMaxZ()))
-def slot=new Cube(0.93,10.5,bracket.getTotalZ()).toCSG()
+def slot=new Cube(1.43,10.5,bracket.getTotalZ()).toCSG()
 		.toZMin()
 
-def setscrew = new Cylinder(5.9/2,40).toCSG()
+def setscrew = new Cylinder(5.9/2,bracket.getTotalX()).toCSG()
 			.movez(-bracket.getTotalX()/2)
 			.roty(90)
 			.toZMin()
@@ -15,4 +15,5 @@ def setscrew = new Cylinder(5.9/2,40).toCSG()
 			
 
 bracket=bracket.difference([slot,setscrew,setscrew.movez(-10)])
+bracket.setName("ArborChiselHolder")
 return bracket
