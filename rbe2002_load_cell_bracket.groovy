51954d5c645ccd750b9b1593705bbc45d07483d4
Kevin Harrington <harrington@wpi.edu>
2019-10-29 10:42:13 -0400

    making bracket

--- rbe2002_load_cell_bracket.groovy
+++ rbe2002_load_cell_bracket.groovy
@@ -1,1 +1,12 @@
 //Your code here
+
+def bracket = new Cube(45,45,2).toCSG()
+				.toZMin()
+				.toXMin()
+				.movex(-3)
+
+def bolt = new Cylinder(1.6,2).toCSG()
+
+def bolts = bolt.movey(3).union(bolt.movey(-3))
+
+return [bracket.difference(bolts)]
