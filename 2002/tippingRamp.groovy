8aa3e38f1813a8153d8704dbe5571d1a8b75d46d
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-26 10:32:04 -0500

    adding tooth

--- 2002/tippingRamp.groovy
+++ 2002/tippingRamp.groovy
@@ -8,31 +8,63 @@ double rampWidth = 500
 double radAngle =Math.tan(Math.toRadians(rampAngle))
 double heightOfPlateAtTipping =radAngle*(rampRun-dowlerCenterToRampEnd)
 double dowelDropcenter = dowelRadius*3
+double toothSpacing =60
+double rampSupportlength = dowlerCenterToRampEnd*2-50
 println "Ramp angle = "+rampAngle+" height "+heightOfPlateAtTipping
 
 def dowel = new Cylinder(dowelRadius,rampWidth).toCSG()
 			.rotx(90)
 			.movey(-rampWidth/2)
 			.movez(heightOfPlateAtTipping-dowelDropcenter)
+def tooth = new Cube(toothSpacing/2,woodThickness,woodThickness).toCSG()
 
 def tippingTopCutout = new Cube(dowlerCenterToRampEnd*2,rampWidth,rampRise).toCSG()
 					.toZMin()
 					.movez(-woodThickness)
 
+
+def tippingFrame = new Transform()
+		.roty(-rampAngle)
+		.movez(heightOfPlateAtTipping)
+def bottomTeeth = []
+def downTooth = tooth.toZMax().toXMax()
+for(Double i=(rampRun-dowlerCenterToRampEnd);i>-dowlerCenterToRampEnd+toothSpacing/2;i-=toothSpacing){
+
+	def moved = downTooth.movex(i)
+	bottomTeeth.add(moved)
+}
+def topTeeth = []
+def upTooth = tooth.toZMax().toXMax()
+			.movey(woodThickness)
+
+for(Double i=(rampSupportlength/2);i>-rampSupportlength/2;i-=toothSpacing){
+
+	def moved = upTooth.movex(i)
+	topTeeth.add(moved)
+}
+
+
+def tippingRib = new Cube(rampSupportlength,woodThickness,dowelDropcenter*2).toCSG()
+					.toZMax()	
+					.movez(-woodThickness)
+					.movey(woodThickness)
+					.union(topTeeth)
+					.transformed(tippingFrame)
+					.difference(dowel)
+def lowerRib = new Wedge(rampRun,woodThickness,rampRise).toCSG()
+				.movey(-woodThickness/2)
+				.movex(-dowlerCenterToRampEnd)
+				.difference(tippingTopCutout
+					.transformed(tippingFrame)
+					)
+				.difference(tippingTopCutout
+					.movez(heightOfPlateAtTipping)
+					)
+				.difference(dowel)
+				.union(bottomTeeth)
 def tippingTop = new Cube(dowlerCenterToRampEnd*2,rampWidth,woodThickness).toCSG()
 					.toZMax()
 					.roty(-rampAngle)
 					.movez(heightOfPlateAtTipping)
-
-def lowerRib = new Wedge(rampRun,woodThickness,rampRise).toCSG()
-				.movex(-dowlerCenterToRampEnd)
-				.difference(tippingTopCutout
-					.roty(-rampAngle)
-					.movez(heightOfPlateAtTipping)
-					)
-				.difference(tippingTopCutout
-					
-					.movez(heightOfPlateAtTipping)
-					)
-
-return [lowerRib,tippingTop,dowel]
+					.difference(tippingRib)
+return [lowerRib,tippingTop,dowel,tippingRib,tooth]
