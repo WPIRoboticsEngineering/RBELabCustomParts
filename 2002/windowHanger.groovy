12d8b2c0a5ba89d6e89f9891206dc424602adb66
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-26 08:18:34 -0500

    window placed

--- 2002/windowHanger.groovy
+++ 2002/windowHanger.groovy
@@ -5,6 +5,7 @@ double woodThickness = 5.35
 def windowHole = new Cube(woodThickness,windowWidth,windowHeight).toCSG()
 def wall = new Cube(woodThickness,windowWidth*3,windowHeight*3).toCSG()
 		.difference(windowHole)
+		.movez(windowHeight/2)
 		.setColor(javafx.scene.paint.Color.CYAN);
 
 
