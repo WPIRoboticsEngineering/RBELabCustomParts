52436693f94c48ff174e15a39e99460ce166458c
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-26 08:17:56 -0500

    window with hole

--- 2002/windowHanger.groovy
+++ 2002/windowHanger.groovy
@@ -1,1 +1,11 @@
-//Your code here
+double windowWidth = 75
+double windowHeight = 100
+double woodThickness = 5.35
+
+def windowHole = new Cube(woodThickness,windowWidth,windowHeight).toCSG()
+def wall = new Cube(woodThickness,windowWidth*3,windowHeight*3).toCSG()
+		.difference(windowHole)
+		.setColor(javafx.scene.paint.Color.CYAN);
+
+
+return [wall]
