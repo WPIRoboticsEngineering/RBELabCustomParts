000caf2ff2ad68c744e64952ac71c28600e951e7
JansemSmith <jansen.michael.smith@gmail.com>
2022-03-28 19:37:30 -0400

    Init commit.

--- Printbed-three-eighths-inch-Spacer.groovy
+++ Printbed-three-eighths-inch-Spacer.groovy
@@ -1,3 +1,17 @@
-//Your code here
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
+
+import eu.mihosoft.vrl.v3d.CSG
 
 CSG vitamin_vexSpacer_threeEighth = Vitamins.get("vexSpacer", "threeEighth")
+
+CSG printbed = vitamin_vexSpacer_threeEighth
+
+String filename = "Printbed-three-eighths-inch-Spacer.stl";
+
+def abs_path = Paths.get(filename)
+
+FileUtil.write(abs_path,
+		printbed.toStlString());
+println "STL EXPORT to "+filename
+println abs_path
+return printbed;
