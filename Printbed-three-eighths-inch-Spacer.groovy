d6806ea07da52493fb3b900f710b68736963262a
JansemSmith <jansen.michael.smith@gmail.com>
2022-03-28 20:59:08 -0400

    filled printbed with spacers

--- Printbed-three-eighths-inch-Spacer.groovy
+++ Printbed-three-eighths-inch-Spacer.groovy
@@ -4,8 +4,17 @@ import eu.mihosoft.vrl.v3d.CSG
 
 CSG vitamin_vexSpacer_threeEighth = Vitamins.get("vexSpacer", "threeEighth")
 
+def dimX = vitamin_vexSpacer_threeEighth.getTotalX() + 5 // add 5mm cushion
+
+
 CSG printbed = vitamin_vexSpacer_threeEighth
 
+for(double i = 0; i<180; i+= dimX){
+	for(double j = 0; j<180; j+= dimX){
+		printbed = printbed.union(vitamin_vexSpacer_threeEighth.movex(i).movey(j))
+	}
+}
+	
 String filename = "Printbed-three-eighths-inch-Spacer.stl";
 
 def abs_path = Paths.get(filename)
